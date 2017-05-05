7e3f419556fcca81826a1318f589bd7c354c0f04
romina <romina.torres.b@gmail.com>
2017-05-05 10:08:16 -0300

    segundo ejercicio - mi squad

---
+++ js/main.js
@@ -0,0 +1,15 @@
+function MiembroSquad(nombre, apellido, edad, hobbies){
+	this.nombre =nombre;
+	this.apellido=apellido;
+	this.edad= edad;
+	this.hobbies = hobbies;
+}
+var vane=new MiembroSquad("Vanessa", "Perez",35,"Hacer Karate");
+var cony=new MiembroSquad("Constanza", "Carrasco",23,"Hacer manualidades");
+var nata=new MiembroSquad("Natalia", "Garrido",25,"Ver animé");
+var naty=new MiembroSquad("Natalia", "Vivanco",27,"Leer");
+var pola=new MiembroSquad("Paola", "Urra",23,"Dormir");
+var kathy=new MiembroSquad("Katherine", "Sandoval",24,"Leer");
+var pauli=new MiembroSquad("Paulina", "Gonzalez",25,"Leer");
+var sou=new MiembroSquad("Sou-Elene", "Cruces",33,"Dormir");
+var mina=new MiembroSquad("Romina", "Torres",31,"Cantar");
